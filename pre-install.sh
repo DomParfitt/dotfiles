294223cd93100be962473e27270a486c77fb294b
Dom Parfitt <dominicparfitt@economist.com>
2020-01-24 14:07:52 +0000

    Added preinstall

---
+++ pre-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Check for Oh My ZSH and install it if it isn't present
+oh_my_zsh="$HOME/.oh-my-zsh"
+if ! [[ -d ${oh_my_zsh} ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    upgrade_oh_my_zsh
+fi
