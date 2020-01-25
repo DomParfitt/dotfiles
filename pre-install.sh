048a637dc34c98c007b3df9d93519d44aff83555
DomParfitt <dom.parfitt@gmail.com>
2020-01-25 11:10:31 +0000

    Fixed rust command

--- pre-install.sh
+++ pre-install.sh
@@ -11,7 +11,7 @@ fi
 # Check for Rust and install it if it isn't present
 rustup="$HOME/.rustup"
 if ! [[ -d "${rustup}" ]]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 else
     rustup update
 fi
