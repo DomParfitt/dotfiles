0e98d0350c6861d81b9fc4c70c585dbaf2c8e65e
DomParfitt <dom.parfitt@gmail.com>
2020-01-26 20:59:36 +0000

    Removed update

--- pre-install.sh
+++ pre-install.sh
@@ -3,15 +3,13 @@
 # Check for Antigen and install it if it isn't present
 antigen="$HOME/antigen.zsh"
 if ! [[ -r "$antigen" ]]; then
-    curl -L git.io/antigen > $HOME/antigen.zsh
+    curl -L git.io/antigen > "$antigen"
 fi
 
 # Check for Oh My ZSH and install it if it isn't present
 oh_my_zsh="$HOME/.oh-my-zsh"
 if ! [[ -d "$oh_my_zsh" ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
-    upgrade_oh_my_zsh
 fi
 
 # sudo chsh -s "$(which zsh)"
