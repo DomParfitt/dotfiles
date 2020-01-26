27f66353f0154145335f5d9ab7ecafaf94be413f
DomParfitt <dom.parfitt@gmail.com>
2020-01-26 21:00:47 +0000

    Moved rust install

--- post-install.sh
+++ post-install.sh
@@ -1,9 +1,1 @@
 #!/bin/bash
-
-# Check for Rust and install it if it isn't present
-rustup="$HOME/.rustup"
-if ! [[ -d "${rustup}" ]]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-else
-    rustup update
-fi
