5e5f202c41ce841e9892043bf5c7cc59aba9c6cc
Dom Parfitt <dominicparfitt@economist.com>
2020-02-17 19:54:50 +0000

    Moved rust installation.

--- post-install.sh
+++ post-install.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# Check for Rust and install it if it isn't present
+rustup="$HOME/.rustup"
+if ! [[ -d "${rustup}" ]]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+else
+    rustup update
+fi
--- pre-install.sh
+++ pre-install.sh
@@ -11,11 +11,3 @@ oh_my_zsh="$HOME/.oh-my-zsh"
 if ! [[ -d "$oh_my_zsh" ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-
-# Check for Rust and install it if it isn't present
-rustup="$HOME/.rustup"
-if ! [[ -d "${rustup}" ]]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-else
-    rustup update
-fi
