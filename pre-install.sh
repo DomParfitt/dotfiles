688c782c51dc3ed5e6d640512a42d629d6993b3a
Dom Parfitt <dominicparfitt@economist.com>
2020-04-08 08:31:42 +0100

    Added check for curl to pre-install.

--- pre-install.sh
+++ pre-install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if curl is installed and exit with message if not
+if ! command -v curl > /dev/null 2>&1; then
+  echo "curl is not installed. You must install it before continuing."
+  exit 1
+fi 
+
 # Check for Antigen and install it if it isn't present
 antigen="$HOME/antigen.zsh"
 if ! [[ -r "$antigen" ]]; then
