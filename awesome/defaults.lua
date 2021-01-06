a737b633a0f5307bda06691a65bc6ac80dfa860c
Dom Parfitt <mail@domparfitt.com>
2021-01-06 17:18:20 +0000

    Remove old directories

--- awesome/defaults.lua
+++
@@ -1,6 +0,0 @@
-local defaults = {}
-
-defaults.terminal = "alacritty"
-defaults.editor = os.getenv("EDITOR") or "vim"
-
-return defaults
--- profile
+++
@@ -1,3 +0,0 @@
-export TERMINAL="$(which alacritty)"
-export FILE_EXPLORER="$(which thunar)"
-export APP_LAUNCHER="xfce4-appfinder"
