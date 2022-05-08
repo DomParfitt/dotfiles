8dc655facb014e22e1774f24dce075a64fb75117
Dom Parfitt <mail@domparfitt.com>
2022-05-08 20:32:25 +0100

    Update fish

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,7 +1,14 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
     
+    # Use prefix search on Up for history
+    bind --key up history-prefix-search-backward    
+    bind \e\[A history-prefix-search-backward
+
+    # Set prompt theme
     starship init fish | source
+
+    set fish_color_command green
+    set fish_color_param white
 end
 
 
