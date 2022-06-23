38a0cc60f25feb52a069eb9a684e9390852d567b
Dom Parfitt <mail@domparfitt.com>
2022-06-23 21:23:28 +0100

    Update fish config

---
+++ roles/fish/files/conf.d/fisher_setup.fish
@@ -0,0 +1,8 @@
+set -U fisher_path ~/.config/fisher
+
+set -a fish_function_path $fisher_path/functions
+set -a fish_complete_path $fisher_path/completions
+
+for file in $fisher_path/conf.d/*.fish
+    source $file
+end
--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
-    
     # Use prefix search on Up for history
     bind --key up history-prefix-search-backward    
     bind \e\[A history-prefix-search-backward
@@ -7,8 +6,10 @@ if status is-interactive
     # Set prompt theme
     starship init fish | source
 
+    # Set colors
     set fish_color_command green
     set fish_color_param white
+    set fish_color_search_match --background=normal
 end
 
 
