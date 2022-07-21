ab1755f7608560e4eb6ff05ab9f48ca07504c20e
Dom Parfitt <mail@domparfitt.com>
2022-07-21 22:27:01 +0100

    Tweak setup script

---
+++ roles/fish/files/completions/setup.fish
@@ -0,0 +1,20 @@
+set --local roles (./setup -l)
+complete -c setup -f -a (string join ' ' $roles)
+
+set --local opts (./setup -h | \
+   # Get the flag info from the usage message
+   sed '1,/options/d' | \
+   # Replace any sequence of spaces with a single space
+   string replace -ar '\s+' ' ' | \
+   # Remove newlines to get rid of extras
+   tr -d \n | \
+   # Reinsert newlines between flags
+   string replace -ar '(-\w),' '\n$1' | \
+   # Split each line into required sections
+   # Should be: -<short-flag> --<long-flag> description
+   string replace -afr '\\-([a-z]+) --([a-z]+[a-z\\-]*) (.*)' '$1,$2,$3')
+
+for option in $opts
+  set --local opt (echo $option | string split ,)
+  complete -c setup -s $opt[1] -l $opt[2] -d $opt[3]
+end
