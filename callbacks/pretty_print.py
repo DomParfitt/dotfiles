5842426f10f0f94c10440cdc195e18d83fcb1aa1
Dom Parfitt <mail@domparfitt.com>
2023-05-11 21:16:18 +0100

    Update output callback

--- callbacks/pretty_print.py
+++ callbacks/pretty_print.py
@@ -1,11 +1,40 @@
 from ansible.plugins.callback import CallbackBase
+from ansible.utils.color import stringc
 from datetime import datetime
 import shutil
+from enum import Enum
 
-RESULT_SUCCESS = "SUCCESS"
-RESULT_CHANGED = "CHANGED"
-RESULT_FAILED = "FAILED"
-RESULT_SKIPPED = "SKIPPED"
+
+class Result(Enum):
+    FAILED = "FAILED"
+    CHANGED = "CHANGED"
+    SUCCESS = "SUCCESS"
+    SKIPPED = "SKIPPED"
+    NONE = ""
+
+    def __str__(self) -> str:
+        return self.value
+
+    def __len__(self) -> int:
+        return len(self.value)
+
+    def color(self):
+        if self == Result.SUCCESS:
+            return 'green'
+        elif self == Result.SKIPPED:
+            return 'blue'
+        elif self == Result.CHANGED:
+            return 'yellow'
+        elif self == Result.FAILED:
+            return 'red'
+        else:
+            return 'bright gray'
+
+    def flatten(results):
+        for result in Result:
+            if result in results:
+                return result
+        return Result.SKIPPED
 
 
 class CallbackModule(CallbackBase):
@@ -13,6 +42,8 @@ class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = 'stdout'
     CALLBACK_NAME = 'pretty_print'
+
+    _show_skipped = False
 
     _current_role = None
     _current_task = None
@@ -26,15 +57,9 @@ class CallbackModule(CallbackBase):
 
     def v2_playbook_on_task_start(self, task, is_conditional):
         if task._role is None:
-            current_task = {
-                "name": task.name,
-                "start": datetime.now()
-            }
-
-            self._current_task = current_task
-            return
-
-        role_name = task._role._role_name
+            role_name = "setup"
+        else:
+            role_name = task._role._role_name
 
         if self._current_role is None:
             self._on_role_start(role_name)
@@ -44,57 +69,85 @@ class CallbackModule(CallbackBase):
             self._on_role_start(role_name)
 
         self._update_parent_stack(task)
-        current_task = {
+
+        self._current_task = {
             "name": task.name,
             "start": datetime.now()
         }
 
-        self._current_task = current_task
+    def v2_playbook_on_stats(self, stats):
+        self._on_role_end()
+        self.printer.print_summary(self._start, self._processed_roles)
 
     def v2_runner_on_ok(self, result):
         if result.is_changed():
-            self._current_role['result'] = RESULT_CHANGED
-            self._on_task_end(RESULT_CHANGED, self._msg_from_result(result))
+            res = Result.CHANGED
         else:
-            self._on_task_end(RESULT_SUCCESS, self._msg_from_result(result))
+            res = Result.SUCCESS
 
-    def v2_runner_on_failed(self, result, ignore_errors=False):
-        self._current_role['result'] = RESULT_FAILED
-        self._on_task_end(RESULT_FAILED, self._msg_from_result(result, True))
+        include = self._include_file_from_result(result)
+        if include is not None:
+            res = Result.NONE
+            self._current_task['display_name'] = f"{self._current_task['name']} ({include})"
 
-    def v2_on_file_diff(self, result):
-        self._on_task_end(RESULT_CHANGED, self._msg_from_result(result))
+        self._on_task_end(res, self._msg_from_result(result))
 
     def v2_runner_on_skipped(self, result):
-        self._on_task_end(RESULT_SKIPPED, self._msg_from_result(result))
+        self._on_task_end(Result.SKIPPED, self._msg_from_result(result))
 
-    def v2_playbook_on_stats(self, stats):
-        self._on_role_end()
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        # self._display.display(str(result._result))
+        self._on_task_end(Result.FAILED, self._msg_from_result(result))
 
-        self.printer.print_summary(self._start, self._processed_roles)
+    def v2_on_file_diff(self, result):
+        self._on_task_end(Result.CHANGED, self._msg_from_result(result))
+
+    def v2_runner_item_on_ok(self, result):
+        # import pprint
+        # loop_var = result._result['ansible_loop_var']
+        # self._display.display("Item OK")
+        # self._display.display(pprint.pformat(result._result[loop_var]))
+        # self._display.display(pprint.pformat(result._result))
+        pass
+
+    def v2_runner_item_on_failed(self, result):
+        pass
+
+    def v2_runner_item_on_skipped(self, result):
+        pass
 
     def _on_role_start(self, role_name):
-        self.printer.print_role_name(role_name)
         self._current_role = {
             "name": role_name,
             "tasks": [],
-            "result": RESULT_SUCCESS,
+            "result": Result.SUCCESS,
             "start": datetime.now()
         }
 
     def _on_role_end(self):
         self._current_role['end'] = datetime.now()
-        self._processed_roles.append(self._current_role)
+        self._current_role['result'] = Result.flatten(
+            [task['result'] for task in self._current_role['tasks']])
+
+        if len(self._current_role['tasks']) != 0:
+            self._processed_roles.append(self._current_role)
 
     def _on_task_end(self, result, msg=None):
         if self._current_task is None:
             return
 
+        if result == Result.SKIPPED and not self._show_skipped:
+            return
+
+        # If this is the first task we've completed for the current role then we need to print the
+        # role name first
+        if len(self._current_role['tasks']) == 0:
+            self.printer.print_role_name(self._current_role['name'])
+
         self._current_task['result'] = result
         self._current_task['end'] = datetime.now()
         self._current_task['msg'] = msg
-        if self._current_role is not None:
-            self._current_role['tasks'].append(self._current_task)
+        self._current_role['tasks'].append(self._current_task)
 
         self.printer.print_task(
             self._current_task, indent_level=len(self._parent_stack) + 1)
@@ -121,127 +174,204 @@ class CallbackModule(CallbackBase):
         if not sameParent:
             self._parent_stack.append(parentTask)
 
-    def _msg_from_result(self, result, force=False):
-        if '_ansible_verbose_always' in result._result or force:
+    def _msg_from_result(self, result):
+        if '_ansible_verbose_always' not in result._result and not result.is_failed():
+            return None
+        
+        if 'results' not in result._result:
             return result._result['msg']
+        
+        failed_results = [result for result in result._result['results'] if result['failed']]
+        
+        msgs = []
+        for fail in failed_results:
+            loop_var = fail['ansible_loop_var']
+            msgs.append({
+                "item": fail[loop_var],
+                "msg": fail['stderr_lines'][0], # fail['msg']
+            })
+        
+        return msgs
 
-        return None
+    def _include_file_from_result(self, result):
+        if 'results' not in result._result:
+            return
+
+        results = result._result['results']
+
+        if len(results) == 0:
+            return
+
+        if 'include' not in results[0]:
+            return
+
+        include = results[0]['include']
+        include = str.split(include, '/')
+        include = include[len(include) - 1]
+        include = str.split(include, '.')
+        return include[0]
 
 
 class Printer:
 
-    def __init__(self, display):
-        self.display = display
-
-    def print_summary(self, start, processed_roles):
-        self.display(f'\n')
-        self.display('-' * self.output_width())
-        self.display(self.bold('SUMMARY'))
-        for role in processed_roles:
-            self._print_role(role)
-
-        self.display('-' * self.output_width())
-        self._print_line(
-            ["TOTAL", "", self._format_duration(start, datetime.now())],
-        )
-        self.display(f'\n')
+    def __init__(self, print):
+        self.print = print
 
     def print_role_name(self, role_name):
-        self.display(f'\n')
-        self.display(self.bold(role_name))
+        self._print_line()
+        self._print_line(role_name, bold=True)
 
-    def _print_role(self, role):
+    def print_task(self, task, indent_level=1):
+        if 'display_name' in task:
+            name = task['display_name']
+        else:
+            name = task['name']
+
+        result = task['result']
+
+        if result == Result.NONE:
+            duration = ''
+        else:
+            start = task['start']
+            end = task['end']
+            duration = Printer._format_duration(start, end)
+
+        color = result.color()
+
+        self._print_line(
+            [name, result, duration],
+            color=color,
+            indent_level=indent_level
+        )
+
+        msg = task['msg']
+        if msg is not None:
+            self._print_msg(msg, indent_level, color)
+
+    def print_summary(self, start, roles):
+        self._print_line()
+        self._print_linebreak()
+        self._print_line(['SUMMARY'], bold=True)
+
+        results = []
+        for role in roles:
+            results.append(role['result'])
+            self._print_role_summary(role)
+
+        result = Result.flatten(results)
+
+        self._print_line()
+        self._print_linebreak()
+        self._print_line(
+            [
+                "OVERALL",
+                {
+                    "text": result,
+                    "color": result.color()
+                },
+                Printer._format_duration(start, datetime.now())
+            ],
+            bold=True,
+        )
+        self._print_line()
+
+    def _print_role_summary(self, role):
         name = role['name']
         result = role['result']
         start = role['start']
         end = role['end']
 
+        duration = Printer._format_duration(start, end)
+
         self._print_line(
-            [name,  result, self._format_duration(start, end)],
-            color=self.result_color(result),
+            [name,  result, duration],
+            color=result.color(),
             indent_level=1
         )
 
-    def print_task(self, task, indent_level=1):
-        name = task['name']
-        result = task['result']
-        msg = task['msg']
-        start = task['start']
-        end = task['end']
-
-        self._print_line(
-            [name, result, self._format_duration(start, end)],
-            color=self.result_color(result),
-            indent_level=indent_level
-        )
-
-        self._print_msg(msg, indent_level, self.result_color(result))
-
     def _print_msg(self, msg, indent_level, color):
-        if msg is None:
-            return
+
+        def print_msg_line(string):
+            self._print_line(
+                "".join(string),
+                color=color,
+                italic=True,
+                indent_level=indent_level + 1)
 
         if isinstance(msg, list):
             for line in msg:
-                self._print_line(
-                    [self.italic("".join(line))],
-                    color=color,
-                    indent_level=indent_level + 1)
+                if isinstance(line, dict):
+                    print_msg_line(f"- Item: {line['item']}")
+                    print_msg_line(f"  Msg:  {line['msg']}")
+                else:
+                    print_msg_line(line)
         else:
-            self._print_line(
-                [self.italic("".join(msg))],
-                color=color,
-                indent_level=indent_level + 1)
+            print_msg_line(msg)
 
-    def _print_line(self, items, color='white', indent_level=0):
-        indent = indent_level * 2
-        line = ""
-        column_size = round(self.output_width() / len(items))
+    def _print_linebreak(self, char='='):
+        self._print_line(char * Printer._output_width())
+
+    def _print_line(self, items=[], color='bright gray', indent_level=0, bold=False, italic=False):
+
+        def format_item(item, color):
+            if bold:
+                item = f'\033[1m{item}\033[0m'
+
+            if italic:
+                item = f'\033[2m{item}\033[0m'
+
+            return f"{stringc(str(item), color)}"
+
+        # Syntactic sugar to allow passing single string
+        if isinstance(items, str):
+            items = [items]
+
+        # Add an extra column so we can give the first one extra space
+        column_count = len(items) + 1
+        column_size = round(Printer._output_width() / column_count)
+
+        line = ''
         for idx, item in enumerate(items):
-            if idx == 0:
-                spaces = indent * ' '
-                line += f'{spaces}{item}{self.spacing(item, desired_spacing=column_size-indent)}'
+            if isinstance(item, dict):
+                text = item['text']
+                item_size = len(text)
+
+                item = format_item(text, item['color'])
             else:
-                line += f'{item}{self.spacing(item, desired_spacing=column_size)}'
+                item_size = len(item)
+                item = format_item(item, color)
 
-        self.display(line.rstrip(), color=color)
+            if idx == 0:
+                available_space = column_size * 2
+                prefix_size = indent_level * 2
+            else:
+                available_space = column_size
+                # prefix_size = 0 # Left justify
+                # prefix_size = available_space - len(item) # Right justify
+                prefix_size = round(
+                    (available_space - item_size) / 2)  # Centre justify
 
-    def bold(self, string):
-        return f'\033[1m{string}\033[0m'
+            prefix = ' ' * prefix_size
+            suffix = ' ' * (available_space - prefix_size - item_size)
 
-    def italic(self, string):
-        return f'\033[2m{string}\033[0m'
+            line += f'{prefix}{item}{suffix}'
 
-    def spacing(self, first_entry, desired_spacing=25):
-        return ' ' * (desired_spacing - len(first_entry))
+        self.print(line.rstrip())
 
-    def output_width(self):
+    def _output_width():
         try:
             return shutil.get_terminal_size().columns
         except:
             return 100
 
-    def result_color(self, result):
-        if result == RESULT_SUCCESS:
-            return 'green'
-        elif result == RESULT_SKIPPED:
-            return 'blue'
-        elif result == RESULT_CHANGED:
-            return 'yellow'
-        elif result == RESULT_FAILED:
-            return 'red'
-        else:
-            return 'white'
-
-    def _format_duration(self, start, end):
-        total_duration = (end - start)
-        seconds = total_duration.total_seconds()
+    def _format_duration(start: datetime, end: datetime):
+        duration = end - start
+        seconds = duration.total_seconds()
         if seconds >= 60:
             seconds_remaining = seconds % 60
             minutes = (seconds - seconds_remaining) / 60
-            duration = "{0:.0f}m{1:.0f}s".format(minutes, seconds_remaining)
+            return "{0:.0f}m{1:.0f}s".format(minutes, seconds_remaining)
         elif seconds >= 1:
-            duration = "{0:.2f}s".format(seconds)
+            return "{0:.2f}s".format(seconds)
         else:
-            duration = "{0:.0f}ms".format(seconds * 1000)
-        return duration
+            return "{0:.0f}ms".format(seconds * 1000)
