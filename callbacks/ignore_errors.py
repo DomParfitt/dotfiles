9d5236d20facb9a4c4b5810e49167d73b9db4ff5
Dom Parfitt <mail@domparfitt.com>
2023-05-15 20:13:29 +0100

    Add enable option

--- callbacks/ignore_errors.py
+++ callbacks/ignore_errors.py
@@ -1,13 +1,30 @@
 from __future__ import print_function
 from ansible.plugins.callback import CallbackBase
 
+DOCUMENTATION = '''
+    name: ignore_errors
+    type: callback
+    description: Sets the `ignore_errors` property of each role in the playbook so that an error in one role will not prevent subsequent roles from running
+    short_description: Sets the `ignore_errors` property of each role in the playbook 
+    options:
+        enabled:
+            name: Enabled
+            description: Enable the plugin.
+            ini:
+                - section: callback_ignore_errors
+                  key: enabled
+            env:
+                - name: ANSIBLE_CALLBACK_IGNORE_ERRORS_ENABLED
+            default: True
+            type: bool
+'''
+
 
 class CallbackModule(CallbackBase):
     def v2_playbook_on_start(self, playbook):
-        """
-        Dynamically set the `ignore_errors` property of each role to `True` so that an error in one role
-        will not prevent subsequent roles from running
-        """
+        if not self.get_option('enabled'):
+            return
+
         plays = playbook.get_plays()
 
         # Note: Although identical roles are shared between plays we cannot deduplicate them,
