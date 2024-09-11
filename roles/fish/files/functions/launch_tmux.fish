a983420269c62c70421ebeb0f7796acf9078ae97
Dom Parfitt <mail@domparfitt.com>
2024-09-11 08:48:27 +0100

    Fix VSCode shell resolution

--- roles/fish/files/functions/launch_tmux.fish
+++ roles/fish/files/functions/launch_tmux.fish
@@ -1,4 +1,16 @@
 function launch_tmux --description "Launch a tmux session if not already in one"
+  # Don't use tmux if we've set an env var to disable it
+  if set --query NO_TMUX
+    echo Not running in tmux as NO_TMUX is set
+    return
+  end
+
+  # Stop VSCode opening tmux as part of its shell resolution
+  if set --query VSCODE_RESOLVING_ENVIRONMENT
+    echo Not running in tmux as VSCODE_RESOLVING_ENVIRONMENT is set
+    return
+  end
+
   # Check tmux is installed
   if not type --query tmux
     return
